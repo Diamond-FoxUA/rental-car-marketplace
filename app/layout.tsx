aebdb3626fabfe88335121e1a31145d9244d36f4
Dmytro Farbun <dimofarb@gmail.com>
2026-03-11 22:08:57 +0200

    feat: add main page content and styles. add hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.variable} ${inter.variable} antialiased`}>
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Link from "next/link";
+import ButtonLink from '@/components/ui/ButtonLink/ButtonLink';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Find your perfect rental car</h1>
-      <p>Reliable and budget-friendly rentals for any journey</p>
-      <Link href='/catalog'>View Catalog</Link>
-    </div>
+    <main className="relative min-h-screen bg-[url('/images/hero.webp')] bg-cover bg-center">
+      <div className="absolute inset-0 bg-black/20"></div>
+      <div className="relative z-10 flex min-h-screen flex-col items-center justify-end gap-[16px] px-auto py-[60px] text-center">
+        <h1>Find your perfect rental car</h1>
+        <p className="text-[24px] font-semibold text-[var(--white)]">
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <ButtonLink className='mt-[24px]' href="/catalog" text="View Catalog" />
+      </div>
+    </main>
   );
 }
