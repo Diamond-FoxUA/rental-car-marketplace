55732b1f65093b73a9f50447d979d821c53ee390
Dmytro Farbun <dimofarb@gmail.com>
2026-03-11 22:09:20 +0200

    feat: add ButtonLink reusable component

---
+++ components/ui/ButtonLink/ButtonLink.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+interface ButtonLinkProps {
+  className?: string;
+  href: string;
+  text: string;
+}
+
+export default function ButtonLink({ className, href, text }: ButtonLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={`flex 
+                  justify-center 
+                  items-center 
+                  w-full 
+                  max-w-[276px] 
+                  h-[44px] 
+                  text-base
+                  font-semibold
+                  text-[var(--white)]
+                  bg-[var(--button)]
+                  rounded-xl
+                  hover:bg-[var(--button-hover)]
+                  transition
+                  duration-300
+                  ease-in-out
+                  ${className}`}
+    >
+      {text}
+    </Link>
+  );
+}
