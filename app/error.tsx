5a9b618125296bdc229ae06c0d41435773367877
Dmytro Farbun <dimofarb@gmail.com>
2026-03-12 22:25:59 +0200

    refactor: unify Button component and replace ButtonForm submit button

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Button from '@/components/ui/Button/Button';
+import Button from "@/components/ui/Button/Button";
 
 export default function Error({
   error,
@@ -8,11 +8,12 @@ export default function Error({
   error: Error;
   reset: () => void;
 }) {
+  console.log(error);
   return (
     <section className="min-h-[calc(100vh-68px)] flex flex-col gap-6 justify-center items-center">
       <h1 className="text-[var(--main)]">Something went wrong.</h1>
       <p>An unexpected error occurred. Please, try again.</p>
-      <Button text="Try again" handleClick={() => reset} />
+      <Button text="Try again" onClick={() => reset()} />
     </section>
   );
 }
--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,32 +1,41 @@
 interface ButtonProps {
-  classname?: string;
   text: string;
-  handleClick: () => void;
+  className?: string;
+  onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 }
 
-export default function Button({ text, classname, handleClick }: ButtonProps) {
+export default function Button({
+  text,
+  className,
+  onClick,
+  type = 'button',
+  disabled = false,
+}: ButtonProps) {
   return (
     <button
-      type="button"
-      onClick={handleClick}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
       className={`flex
-         justify-center
-         items-center
-         w-full
-         max-w-[276px]
-         h-[44px]
-         text-base
-         font-semibold
-         text-[var(--white)]
-         bg-[var(--button)]
-         rounded-xl
-         hover:bg-[var(--button-hover)]
-         transition
-         duration-300
-         ease-in-out
-         active:scale-95
-         disabled:opacity-50
-         ${classname}`}
+        justify-center
+        items-center
+        w-full
+        max-w-[276px]
+        h-[44px]
+        text-base
+        font-semibold
+        text-[var(--white)]
+        bg-[var(--button)]
+        rounded-xl
+        hover:bg-[var(--button-hover)]
+        transition
+        duration-300
+        ease-in-out
+        active:scale-95
+        disabled:opacity-50
+        ${className}`}
     >
       {text}
     </button>
