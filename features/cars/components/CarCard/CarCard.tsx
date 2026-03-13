f883ebf83eb5fe713592ab26e731f89ec66da0e2
Dmytro Farbun <dimofarb@gmail.com>
2026-03-13 20:46:48 +0200

    feat: implement styles for cards
    - wrap image with car page link
    
    - add transitions and hover to image

--- features/cars/components/CarCard/CarCard.tsx
+++ features/cars/components/CarCard/CarCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Car } from '../../types/car';
 
 import ButtonLink from '@/components/ui/ButtonLink/ButtonLink';
@@ -8,24 +9,51 @@ interface Props {
 }
 
 export default function CarCard({ car }: Props) {
+  const [, city, country] = car.address.split(', ');
+  const formattedMileage = car.mileage.toLocaleString('uk-UA');
+
   return (
-    <li className='flex flex-col justify-between mb-[80px]'>
+    <li className="flex flex-col justify-between mb-[80px]">
       <div>
-        <Image width={276} height={268} src={car.img} alt={car.description} />
+        <Link
+          className="cursor-pointer hover:scale-102"
+          href={`/catalog/${car.id}`}
+        >
+          <Image
+            className="rounded-[14px] h-[268px] object-cover mb-[16px] hover:scale-102 transition duration-300 ease-in-out"
+            width={276}
+            height={268}
+            src={car.img}
+            alt={car.description}
+          />
+        </Link>
       </div>
-      <div>
-        <h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-[16px] font-[500] leading-[125%]">
           {car.brand} {car.model} {car.year}
         </h2>
-        <p>{car.rentalPrice}</p>
+        <p className="text-[var(--main)] mr-[11px]">${car.rentalPrice}</p>
       </div>
-      <div>
-        <p>{car.address}</p>
-        <p>{car.rentalCompany}</p>
-      </div>
-      <div>
-        <p>{car.type}</p>
-        <p>{car.mileage}</p>
+      <div className="mb-[28px]">
+        <ul>
+          <li className="inline-block text-[12px] font-normal text-[var(--gray)] pr-[6px] border-r border-[var(--gray-light)]">
+            <p className="inline-block text-inherit">{city}</p>
+          </li>
+          <li className="inline-block text-[12px] font-normal text-[var(--gray)] pr-[6px] pl-[6px] border-r border-[var(--gray-light)]">
+            <p className="inline-block text-inherit">{country}</p>
+          </li>
+          <li className="inline-block text-[12px] font-normal text-[var(--gray)] pr-[6px] pl-[6px] border-r border-[var(--gray-light)]">
+            <p className="inline-block text-inherit">{car.rentalCompany}</p>
+          </li>
+        </ul>
+        <ul>
+          <li className="inline-block text-[12px] font-normal text-[var(--gray)] pr-[6px] border-r border-[var(--gray-light)]">
+            <p className="inline-block text-inherit">{car.type}</p>
+          </li>
+          <li className="inline-block text-[12px] font-normal text-[var(--gray)] pl-[6px]">
+            <p className="inline-block text-inherit">{formattedMileage} km</p>
+          </li>
+        </ul>
       </div>
 
       <ButtonLink href={`/catalog/${car.id}`} text="Read more" />
