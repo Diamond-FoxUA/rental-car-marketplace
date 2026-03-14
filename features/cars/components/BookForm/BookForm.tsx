dfbd64c512f250d4d21c5736f61f2e7c5134560f
Dmytro Farbun <dimofarb@gmail.com>
2026-03-14 22:53:52 +0200

    feat: implement booking form validation and submission handling

--- features/cars/components/BookForm/BookForm.tsx
+++ features/cars/components/BookForm/BookForm.tsx
@@ -1,6 +1,48 @@
-import Button from "@/components/ui/Button/Button";
+'use client';
+
+import { useState } from 'react';
+
+import Button from '@/components/ui/Button/Button';
 
 export default function BookForm() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    date: '',
+    comment: '',
+  });
+  const [error, setError] = useState('');
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+
+    setError('');
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!form.name || !form.email) {
+      setError('Please fill in the required fields: Name and Email.');
+      return;
+    }
+
+    setError('');
+
+    alert('Car booked successfully!');
+    setForm({
+      name: '',
+      email: '',
+      date: '',
+      comment: '',
+    });
+  };
+
   return (
     <div className="flex flex-col gap-[24px] p-[32px] border border-[var(--gray-light)] rounded-[10px]">
       <div className="flex flex-col gap-[8px]">
@@ -9,13 +51,50 @@ export default function BookForm() {
           Stay connected! We are always ready to help you.
         </p>
       </div>
-      <form className="flex flex-col gap-[16px]" action="">
-        <input className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]" type="text" placeholder="Name*"/>
-        <input className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]" type="text" placeholder="Email*"/>
-        <input className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]" type="text" placeholder="Booking date"/>
-        <textarea className="!text-[var(--gray)] text-left w-full max-w-full h-[88px] py-[14px] px-[20px] bg-[var(--inputs)] border border-transparent rounded-[12px] placeholder:text-[var(--gray)] resize-none" name="" id="" placeholder="Comment"></textarea>
 
-        <Button className="max-w-[156px] mx-auto mt-[8px]" text="Send" />
+      {error && (
+        <p className='text-[#FF5733] font-[500]'>{error}</p>
+      )}
+
+      <form className="flex flex-col gap-[16px]" onSubmit={handleSubmit}>
+        <input
+          value={form.name}
+          onChange={handleChange}
+          name="name"
+          type="text"
+          placeholder="Name*"
+          className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]"
+        />
+        <input
+          value={form.email}
+          onChange={handleChange}
+          name="email"
+          type="text"
+          placeholder="Email*"
+          className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]"
+        />
+        <input
+          value={form.date}
+          onChange={handleChange}
+          name="date"
+          type="text"
+          placeholder="Booking date"
+          className="!text-[var(--gray)] text-left w-full max-w-full h-[48px] py-[14px] px-[20px] border border-transparent rounded-[12px] placeholder:text-[var(--gray)]"
+        />
+        <textarea
+          value={form.comment}
+          onChange={handleChange}
+          name="comment"
+          id="comment"
+          placeholder="Comment"
+          className="!text-[var(--gray)] text-left w-full max-w-full h-[88px] py-[14px] px-[20px] bg-[var(--inputs)] border border-transparent rounded-[12px] placeholder:text-[var(--gray)] resize-none"
+        ></textarea>
+
+        <Button
+          type="submit"
+          className="max-w-[156px] mx-auto mt-[8px]"
+          text="Send"
+        />
       </form>
     </div>
   );
