df4ccf23a89bba4488cdc4c1a7de6850f6183176
Dmytro Farbun <dimofarb@gmail.com>
2026-03-14 23:23:39 +0200

    feat: add image loading state with loader for car preview

--- features/cars/components/CarInfo/CarInfo.tsx
+++ features/cars/components/CarInfo/CarInfo.tsx
@@ -1,4 +1,9 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
+
+import Loader from '@/components/ui/Loader/Loader';
 
 import { Car } from '../../types/car';
 interface Props {
@@ -9,15 +14,23 @@ import CarDetails from '../CarDetails/CarDetails';
 import BookForm from '../BookForm/BookForm';
 
 export default function CarInfo({ car }: Props) {
+  const [imageLoading, setImageLoading] = useState(true);
+
   return (
     <div className="py-[84px] px-[120px] grid grid-cols-1 lg:grid-cols-[640px_1fr] gap-12">
       <div>
+        {imageLoading && (
+          <div className="absolute w-[640px] h-[512px] bg-[var(--gray-light)] rounded-[14px]">
+            <Loader />
+          </div>
+        )}
         <Image
           className="rounded-[14px] object-cover mb-[40px]"
           src={car.img}
           alt={car?.brand}
           width={640}
           height={512}
+          onLoad={() => setImageLoading(false)}
         ></Image>
         <BookForm />
         {/* <div className="w-[640px] h-[488px] bg-[var(--main)]"></div> */}
